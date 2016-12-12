a9ad794602d410231403265267a5b3ffb7757a03
Takumi Ooshiro <e165703@ie.u-ryukyu.ac.jp>
2016-12-12 23:39:21 +0900

    a

--- src/jp/ac/uryukyu/ie/e165703/Exception.java
+++ src/jp/ac/uryukyu/ie/e165703/Exception.java
@@ -6,7 +6,13 @@ package jp.ac.uryukyu.ie.e165703;
 public class Exception {
     public static void main(String[] args){
         String str = null;
-        int length = str.length();
-        System.out.println(length);
+        try {
+            int length = str.length();
+            System.out.println(length);
+        }
+        catch(NullPointerException e){
+            System.out.println("エラー:NullPointerExceptionが発生しました");
+            e.printStackTrace();
+        }
     }
 }
