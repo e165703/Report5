c63dcea262072a739707aaaa56fa17fac794b5e2
Takumi Ooshiro <e165703@ie.u-ryukyu.ac.jp>
2016-12-12 22:42:45 +0900

    a

---
+++ src/jp/ac/uryukyu/ie/e165703/Exception.java
@@ -0,0 +1,12 @@
+package jp.ac.uryukyu.ie.e165703;
+
+/**
+ * Created by e165703 on 2016/12/12.
+ */
+public class Exception {
+    public static void main(String[] args){
+        String str = null;
+        int length = str.length();
+        System.out.println(length);
+    }
+}
