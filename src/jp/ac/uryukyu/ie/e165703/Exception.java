4b435d44cfeb3c76d7316fab1dc99f08c5071cf8
Takumi Ooshiro <e165703@ie.u-ryukyu.ac.jp>
2016-12-13 00:30:35 +0900

    a

--- src/jp/ac/uryukyu/ie/e165703/Exception.java
+++ src/jp/ac/uryukyu/ie/e165703/Exception.java
@@ -1,11 +1,8 @@
 package jp.ac.uryukyu.ie.e165703;
-
-/**
- * Created by e165703 on 2016/12/12.
- */
-public class Exception {
+public class Exception{
     public static void main(String[] args){
-        String str = null;
+        String str ="3．14";
+        Double.parseDouble(str);
         try {
             int length = str.length();
             System.out.println(length);
